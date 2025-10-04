5f56541fdbeaf514e7e22566b8541686ff0e6338
rabab003 <bintaliraq4@gmail.com>
2025-10-04 10:09:57 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,3 @@
-// import Header1 from "./Components/header/Header1";
-// import Header2 from "./Components/header/Header2";
-// import Header3 from "./Components/header/Header3";
-// import { CssBaseline, ThemeProvider } from "@mui/material";
-// import { ColorModeContext, useMode } from "./theme";
-
-// function App() {
-//   const [theme, colorMode] = useMode();
-
-//   return (
-//     <div className="m-0">
-//       <ColorModeContext.Provider
-//         // @ts-ignore
-//         value={colorMode}
-//       >
-//         <ThemeProvider
-//           // @ts-ignore
-//           theme={theme}
-//         >
-//           <Header1 />
-//           <Header2 />
-//           <Header3 />
-//         </ThemeProvider>
-//       </ColorModeContext.Provider>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// // 29:00
-
 import Header1 from "./Components/header/Header1";
 import Header2 from "./Components/header/Header2";
 import Header3 from "./Components/header/Header3";
