e0e011ee1b8ea6a40395dfd1cf69d6a88809fa7d
rabab003 <bintaliraq4@gmail.com>
2025-10-27 19:10:18 +0300

    add the modal next section

--- src/Components/scroll/ScrollToTop.jsx
+++ src/Components/scroll/ScrollToTop.jsx
@@ -1,12 +1,14 @@
 import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material'
-import { Fab, Zoom } from '@mui/material'
+import { Fab, Zoom, useScrollTrigger } from '@mui/material'
 import React from 'react'
 
 const ScrollToTop = () => {
     return (
         <div>
-            <Zoom in={true}>
-                <Fab size='small' sx={{ position: "fixed", bottom: 33, right: 33 }} color='primary' aria-label='add'>
+            <Zoom in={useScrollTrigger({ threshold: 300 })}>
+                <Fab onClick={() => {
+                    window.scrollTo(0, 0);
+                }} size='small' sx={{ position: "fixed", bottom: 33, right: 33 }} color='primary' aria-label='add'>
                     <KeyboardArrowUp />
                 </Fab>
             </Zoom>
